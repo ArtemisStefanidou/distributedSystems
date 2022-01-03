c18e7a996eede899b8cb486105f7b4d10a3226b2
Bi11 <anxheldohyskaj@gmail.com>
2022-01-03 20:37:45 +0200

    Start

---
+++ src/main/java/org/hua/dit/ditributedsystems/distibutedsystems/DistibutedSystemsApplication.java
@@ -0,0 +1,13 @@
+package org.hua.dit.ditributedsystems.distibutedsystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistibutedSystemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistibutedSystemsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hua/dit/ditributedsystems/distibutedsystems/DistibutedSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hua.dit.ditributedsystems.distibutedsystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistibutedSystemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
