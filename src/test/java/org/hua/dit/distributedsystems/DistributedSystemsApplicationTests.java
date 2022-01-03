deaaa822bacfce00a1a312468be3d38f3fbf03fe
Bi11 <anxheldohyskaj@gmail.com>
2022-01-04 00:03:50 +0200

    change

---
+++ src/test/java/org/hua/dit/distributedsystems/DistributedSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hua.dit.distributedsystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedSystemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
