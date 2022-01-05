4ded10297fc2488c866036071de2c1fdf349e9cc
meletis <meletistsesmelis452001@gmail.com>
2022-01-06 01:24:39 +0200

    7 fields added

--- src/main/java/org/hua/dit/distributedsystems/models/post/UserPost.java
+++ src/main/java/org/hua/dit/distributedsystems/models/post/UserPost.java
@@ -1,4 +1,71 @@
 package org.hua.dit.distributedsystems.models.post;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+
 public class UserPost {
+
+    private Long user_id ;
+    private String user_email ;
+    private int user_phone_number  ;
+    private int user_password  ;
+    private String user_fullname  ;
+    private String user_role  ;
+    private String user_teacher  ;
+
+    public Long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Long user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUser_email() {
+        return user_email;
+    }
+
+    public void setUser_email(String user_email) {
+        this.user_email = user_email;
+    }
+
+    public int getUser_phone_number() {
+        return user_phone_number;
+    }
+
+    public void setUser_phone_number(int user_phone_number) {
+        this.user_phone_number = user_phone_number;
+    }
+
+    public int getUser_password() {
+        return user_password;
+    }
+
+    public void setUser_password(int user_password) {
+        this.user_password = user_password;
+    }
+
+    public String getUser_fullname() {
+        return user_fullname;
+    }
+
+    public void setUser_fullname(String user_fullname) {
+        this.user_fullname = user_fullname;
+    }
+
+    public String getUser_role() {
+        return user_role;
+    }
+
+    public void setUser_role(String user_role) {
+        this.user_role = user_role;
+    }
+
+    public String getUser_teacher() {
+        return user_teacher;
+    }
+
+    public void setUser_teacher(String user_teacher) {
+        this.user_teacher = user_teacher;
+    }
 }
