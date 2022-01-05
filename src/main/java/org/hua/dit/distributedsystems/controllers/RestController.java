8bef22063ed4f3ce93a98c8cf186d9570418a72c
Artemis Stefanidou <rouliscat2000@gmail.com>
2022-01-05 20:04:34 +0200

    /endpoints

--- src/main/java/org/hua/dit/distributedsystems/controllers/RestController.java
+++ src/main/java/org/hua/dit/distributedsystems/controllers/RestController.java
@@ -10,12 +10,33 @@ public class RestController {
 
 //ENERGEIES KATHIGHTH
 
+    // /menuTeacher --> central menu after the sign in from a teacher role=="teacher" (get)
+
     // /createClass --> form for create a class (post)
 
     // /updateClass --> form for update the class (post)
 
     // /deleteClass --> pop up for confirmation to delete the class (delete)
 
+    // /menuClass
+
+    // /createChapter --> form for create a chapter (post)
+
+    // /updateChapter --> form for update the Chapter (patch)
+
+    // /deleteChapter --> pop up for confirmation to delete the Chapter (delete)
+
+    // /menuChapter
+
+    // /createQuestion --> form for create a Question (post)
+
+    // /updateQuestion --> form for update the Question (patch)
+
+    // /deleteQuestion --> pop up for confirmation to delete the Question (delete)
+
+    // /menuQuestion
+
+    // /question --> two endpoints but one for create (post) and the other for update (patch)
 
     // KATI
 }
