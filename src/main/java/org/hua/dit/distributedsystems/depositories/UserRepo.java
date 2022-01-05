123a2bdf599a935f98d5bb32ce1bd73d065b2b33
Bi11 <anxheldohyskaj@gmail.com>
2022-01-05 18:26:58 +0200

    user repo

--- src/main/java/org/hua/dit/distributedsystems/depositories/UserRepo.java
+++ src/main/java/org/hua/dit/distributedsystems/depositories/UserRepo.java
@@ -2,10 +2,9 @@ package org.hua.dit.distributedsystems.depositories;
 
 import org.hua.dit.distributedsystems.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface UserRepo extends JpaRepository<User, Long> {
-
-
 
 }
