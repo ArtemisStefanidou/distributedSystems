dd3bed838c67f4ebc49b5f0db74af408bd23f008
Bi11 <anxheldohyskaj@gmail.com>
2022-01-09 13:35:41 +0200

    fuck fuck fuck 🤬🤬🤬

--- src/main/java/org/hua/dit/distributedsystems/service/UserService.java
+++ src/main/java/org/hua/dit/distributedsystems/service/UserService.java
@@ -1,14 +1,14 @@
-//package org.hua.dit.distributedsystems.service;
-//
-//import org.hua.dit.distributedsystems.models.User;
-//
-//import java.util.List;
-//
-//public interface UserService {
-//
-//    User saveUser(User user);
-//    User getUser(String username);
-//    List<User> getUsers();
-//
-//    public void addRoleToUser(String email, String roleName);
-//}
+package org.hua.dit.distributedsystems.service;
+
+import org.hua.dit.distributedsystems.models.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    User saveUser(User user);
+    User getUser(String username);
+    List<User> getUsers();
+
+    public void addRoleToUser(String email, String roleName);
+}
