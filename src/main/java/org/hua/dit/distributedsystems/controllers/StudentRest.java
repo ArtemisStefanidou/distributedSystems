8819c7af11224fd5a28350fb4d4304fddbbe3f12
meletis <meletistsesmelis452001@gmail.com>
2022-01-10 22:37:52 +0200

    teacher html changed

--- src/main/java/org/hua/dit/distributedsystems/controllers/StudentRest.java
+++ src/main/java/org/hua/dit/distributedsystems/controllers/StudentRest.java
@@ -1,7 +1,9 @@
 package org.hua.dit.distributedsystems.controllers;
 
 import org.hua.dit.distributedsystems.models.post.ClassPost;
+import org.hua.dit.distributedsystems.models.post.GradePost;
 import org.hua.dit.distributedsystems.models.post.QuestionPost;
+import org.hua.dit.distributedsystems.models.post.SubjectPost;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,17 +26,31 @@ public class StudentRest {
             MediaType.APPLICATION_JSON_VALUE
     })
 
-    public void findquizPost(@RequestBody QuestionPost questionPost) {
+    public void findquizPost(@RequestBody QuestionPost questionPost, ClassPost classPost, SubjectPost subjectPost) {
 
         //todo
-        System.out.println(""+questionPost.getQuestion_id());
+        System.out.println("Question:"+questionPost.getQuestion_id() + "ClassName:" +classPost.getClass_name() +"ClassID:"+classPost.getClass_id()
+                +"Subject Name"+subjectPost.getSubject_name() + "Subject ID" +subjectPost.getSubject_id());
 
     }
 
     //  /doQuiz/info (get) emfanish me bash tis plhrofories
+
+
     //  /doQuiz/info/quiz (get) epilogh quiz
-    //  /doQuiz/info/quiz (post) kataxwrhsh apanthsewn sto quiz
-    //  /doQuiz/info/quiz/grade (get) emfanish batmou
+    //  /findquiz/quizanswers (post) kataxwrhsh apanthsewn sto quiz
+    @PostMapping(value="guizanswers" , consumes = {
+            MediaType.APPLICATION_JSON_VALUE
+    })
+
+    public void guizanswers(@RequestBody GradePost gradePost) {
+
+        //todo edw gurnaw apla ton bathmo sto question thelei ftiaksimo
+        System.out.println(gradePost.getGrade_of_question());
+
+    }
+
+    //  /doQuiz/info/quiz/grade (get) emfanish bathmou
 
     // /myGrades (get) emfanish bathologiwn se ola ta quiz pou exei kanei
 }
